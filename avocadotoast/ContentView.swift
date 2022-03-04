33b84338900cedf062480bacbfa894c348c7bade
Imran Razak <Imranrazak1@icloud.com>
2022-03-04 17:36:07 +0000

    Initial Commit

---
+++ avocadotoast/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  avocadotoast
+//
+//  Created by Imran razak on 04/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
